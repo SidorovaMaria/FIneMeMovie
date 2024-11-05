99b727f032d66a6ac01d3c7041ca77c300668888
maria sidorova <sidmashav@icloud.com>
2024-11-05 16:27:53 +0000

    Removed env, updated styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color: { background: "var(--background)" },
+        accent: {
+          1: "var(--accent1)",
+          2: "var(--accent2)",
+          3: "var(--accent3)",
+        },
+      },
+      fontFamily: {
+        fancy: "var(--font-fascinate)",
+        nunito: "var(--font-nunito-sans)",
+      },
+    },
   },
   plugins: [],
 };
